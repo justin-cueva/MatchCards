7782b8664d0e6c4205cb366e3fbbce34fbeff976
justincueva02 <justincueva02@gmail.com>
2022-05-03 13:48:48 -0700

    displaying a form

--- src/components/create/Create.tsx
+++ src/components/create/Create.tsx
@@ -1,5 +1,23 @@
+import "../../styles/create.css";
+
 const Create = () => {
-  return <div style={{ backgroundColor: "#fff" }}>Create</div>;
+  return (
+    <form className="create-page">
+      <h2>Create a new deck</h2>
+      {[1, 2, 3].map((index) => (
+        <div key={index} className="card">
+          <div className="field">
+            <input className="card__input" />
+            <label className="card__label">TERM</label>
+          </div>
+          <div className="field">
+            <input className="card__input" />
+            <label className="card__label">DEFINITION</label>
+          </div>
+        </div>
+      ))}
+    </form>
+  );
 };
 
 export default Create;
