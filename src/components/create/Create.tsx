a1e56a288363ddcf26187d585a183235e22faa46
justincueva02 <justincueva02@gmail.com>
2022-05-03 18:20:20 -0700

    styled the cards

--- src/components/create/Create.tsx
+++ src/components/create/Create.tsx
@@ -1,3 +1,7 @@
+import { BsFillTrashFill } from "react-icons/bs";
+import { MdDragHandle } from "react-icons/md";
+import { HiPlus } from "react-icons/hi";
+
 import "../../styles/create.css";
 
 const Create = () => {
@@ -6,6 +10,17 @@ const Create = () => {
       <h2>Create a new deck</h2>
       {[1, 2, 3].map((index) => (
         <div key={index} className="card">
+          <div className="toolbar">
+            <label className="card-number">{index}</label>
+            <div className="card-icons">
+              <span className="icon--drag-handle">
+                <MdDragHandle />
+              </span>
+              <span className="icon--trash">
+                <BsFillTrashFill />
+              </span>
+            </div>
+          </div>
           <div className="field">
             <input className="card__input" />
             <label className="card__label">TERM</label>
@@ -16,6 +31,14 @@ const Create = () => {
           </div>
         </div>
       ))}
+      <div className="add-card">
+        <span className="border-bottom">
+          <span className="icon--add">
+            <HiPlus />
+          </span>
+          ADD CARD
+        </span>
+      </div>
     </form>
   );
 };
