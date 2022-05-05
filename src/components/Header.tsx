ab74bc39a11ef67553adc95c6693bea7750398b2
justincueva02 <justincueva02@gmail.com>
2022-05-05 12:08:48 -0700

    made header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import { Fragment, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 import MenuModal from "./MenuModal";
+import { links } from "./MenuModal";
 import "../styles/header.css";
 
 const Header = () => {
+  const navigate = useNavigate();
   const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false);
 
   return (
@@ -19,6 +22,20 @@ const Header = () => {
         >
           <GiHamburgerMenu />
         </span>
+        <ul className="link-list">
+          {links.map((link, index) => {
+            return (
+              <li
+                key={index}
+                onClick={() => {
+                  navigate(link.to);
+                }}
+              >
+                {link.name}
+              </li>
+            );
+          })}
+        </ul>
         <button className="btn--auth">Log out</button>
       </nav>
     </Fragment>
