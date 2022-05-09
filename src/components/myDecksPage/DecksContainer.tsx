def728577d4cc8af2ed913656b6574c407e51f3c
justincueva02 <justincueva02@gmail.com>
2022-05-09 12:50:06 -0700

    displaying our decks

--- src/components/myDecksPage/DecksContainer.tsx
+++ src/components/myDecksPage/DecksContainer.tsx
@@ -1,5 +1,28 @@
+import "../../styles/myDecksContainer.css";
+
 const DecksContainer = () => {
-  return <div className="container--my-decks"></div>;
+  return (
+    <div className="container--my-decks">
+      <div className="container--decks-of-a-date">
+        <label>
+          <span>March 20 2022</span>
+        </label>
+        <div className="my-decks__deck">
+          <span>3 terms</span>
+          <h4>Presidents</h4>
+        </div>
+      </div>
+      <div className="container--decks-of-a-date">
+        <label>
+          <span>March 20 2022ch 20 2022ch 20 2022</span>
+        </label>
+        <div className="my-decks__deck">
+          <span>3 terms</span>
+          <h4>Presidents</h4>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DecksContainer;
